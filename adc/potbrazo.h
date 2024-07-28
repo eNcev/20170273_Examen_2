970119f37c5af235056cff45a18b65eba195b952
eNcev <nestorlcevallosc@gmail.com>
2024-07-28 13:12:47 -0500

    Desarrollo: main.c V1

--- adc/potbrazo.h
+++ adc/potbrazo.h
@@ -2,4 +2,4 @@
 #include "../incFiles/stm32f103x6.h"
 
 void initADC(void);
-uint16_t leerPuerto(void);
+uint16_t leerPuerto(uint8_t channel);
--- main.c
+++ main.c
@@ -4,37 +4,93 @@
 #include <string.h>
 #include "uart/uartbrazo.h"
 #include "adc/potbrazo.h"
-
+#include "pwm/servobrazo.h"
 
 int main()
 {
-    uint16_t valorLectura = 0;
-    uint8_t i = 2;
-    uint32_t angulo_inicial = 600;
-	uint32_t angulo_final = 2400;
-    uint32_t anguloADC = 0;
-    TIM4_Init();
+    uint8_t valorLectura;
+    uint8_t i = 0;
+    uint32_t valorADC = 0;
+
+    uint8_t ultimoValorLectura;
+
+    // Inicialización del temporizador 3, del ADC 1 y del UART3
+    TIM3_Init();
     initADC();
     inicializarUart();
-    enviarPalabra("BIENVENIDOS AL PROGRAMA DEL BRAZO ROBOTICO");
-    while(1)
-    {
-        while(i < 7)
-        {
-            valorLectura = leerADC(i);
-            anguloADC = angulo_inicial + ((valorLectura*(angulo_final - angulo_inicial))/4096);
-            modificarDuty(i,anguloADC);
 
-            if(i == 3)
+    enviarPalabra("BIENVENIDOS AL PROGRAMA DEL BRAZO ROBOTICO");
+
+    while(1){
+        
+        valorLectura = leerCaracter();
+        i = 0;
+
+        // Se usa un Do-While para no interrumpir el código que se este ...
+        // ejecutando, en caso se detecte un nuevo caracter. Para ello, ...
+        // se monitorea el estado del bit 5 del registro USART_SR
+        do
+        {
+            if (valorLectura == "a")
             {
-                i = i + 2;
+                // En caso se seleccione la opción "a", solo por una vez ...
+                // se enviara el mensaje de que el Brazo se encuentra ...
+                // inhabilitado
+                if (i = 0){
+                    enviarPalabra("Opción A: Brazo robótico inhabilitado.");
+                    i++;
+                }
+            }
+
+            else if (valorLectura == "b")
+            {
+                // Se utiliza un bucle for, porque al encontrarse dentro ...
+                // de un do-while y manteniendo el caracter "b", siempre ...
+                // entrara en esta selectiva, repitiendo el ciclo hasta ...
+                // que se tenga un nuevo caracter en el UART
+                for(i=0; i < 4; i++)
+                {
+                    // Lectura de los puertos ADC (0; 1; 2 y 3)
+                    valorADC = leerPuerto(i);
+
+                    // Se pasa el valor leido en el Puerto ADC i a la ...
+                    // funcion cambiarAngulo, donde internamente hace ...
+                    // la conversion del valorADC al del contador.
+                    // (resolucion ADC: 4096 => valor max: 10000)
+
+                    // Se aumenta en 1 el valor de "i" debido a que ...
+                    // los canales del temporizador 3 son: 1; 2; 3 y 4
+                    cambiarAngulo(i+1,valorADC);
+                }
+            }
+
+            else if (valorLectura == "g")
+            {
+
+            }
+            else if (valorLectura == "h")
+            {
+
+            }
+            else if (valorLectura == "i")
+            {
+
             }
             else
             {
-                i = i + 1;
+                // En caso se envie un caracter incorrecto, se envia un mensaje ...
+                // de error. Ademas, se vuelve a usar el caracter anterior al error, ...
+                // para mantener el algoritmo que se estaba ejecutando
+                enviarPalabra("Caracter INCORRECTO. Intentar nuevamente.");
+                valorLectura = ultimoValorLectura;
             }
-        }
-        i = 2;
+        }while(!(USART1->SR & (1<<5)));
+
+        // En esta variable se guarda el ultimo caracter leido y que sea correcto. ...
+        // De este modo no se interrumpa el algoritmo actual en caso se envie ...
+        // un caracter incorrecto
+        ultimoValorLectura = valorLectura;
     }
+
     return 0;
 }
